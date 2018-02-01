eb48495a68b62e9f3899543929b654f0fb2c211b
Muthomi Mate <muthomimate@gmail.com>
2018-02-01 16:19:47 +0300

    [Feature-#154325747]add admin controller test and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
-  resources 'users'
+  # resources 'users'
+  get '/admin', to: 'admin#index', as: 'admin_index'
+
 end
