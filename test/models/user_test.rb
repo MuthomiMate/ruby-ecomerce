f1118f7855dcf92d13083ecd37f4f60d8cea401e
Muthomi Mate <muthomimate@gmail.com>
2018-02-01 16:14:00 +0300

    [Feature-#154325747]add devise model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,20 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-	def setup
-		@subject = User.new
-	end
+    def setup
+        @subject = User.new
+    end
 
-	test "it validates email address" do
-		@subject.valid?
-		assert_includes(@subject.errors[:email], "can't be blank")
-	end
-	# should validate_presence_of(:email)
-
-	test "the email provided is valid" do
-		@subject.email = "muthomi"
-		@subject.valid?
-		assert_includes(@subject.errors[:email], "is invalid")
-	end
-
+    test "it validates email address" do
+        @subject.valid?
+        assert_includes(@subject.errors[:email], "can't be blank")
+    end
+    
+    # should validate_presence_of(:email)
+    test "the email provided is valid" do
+        @subject.email = "muthomi"
+        @subject.valid?
+        assert_includes(@subject.errors[:email], "is invalid")
+    end
 end
